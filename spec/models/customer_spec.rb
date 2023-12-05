987808505fe8a8b7adb7626c4d551dec4c31b5e5
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2023-12-05 20:21:29 +0300

    fixed tests in spec/models/customer_spec.rb

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,86 +1,48 @@
 require 'rails_helper'
 
-RSpec.describe "CustomersControllers", type: :request do
-  describe "get customers_path" do
-    it "renders the index view" do
-      FactoryBot.create_list(:customer, 10)
-      get customers_path
-      expect(response).to render_template(:index)
-    end
+RSpec.describe Customer, type: :model do
+  subject { Customer.new(first_name: "Jack", last_name: "Smith", phone: "8889995678", email: "jsmith@sample.com" )}
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
   end
 
-  describe "get customer_path" do
-    it "renders the :show template" do
-      customer = FactoryBot.create(:customer)
-      get customer_path(id: customer.id)
-      expect(response).to render_template(:show)
-    end
-
-    it "redirects to the index path if the customer id is invalid" do
-      get customer_path(id: 5000) # an ID that doesn't exist
-      expect(response).to redirect_to customers_path
-    end
+  it "is not valid without a first_name" do
+    subject.first_name = nil
+    expect(subject).to_not be_valid
   end
 
-  describe "get new_customer_path" do
-    it "renders the :new template" do
-      get new_customer_path
-      expect(response).to render_template(:new)
-    end
+  it "is not valid without a last_name" do
+    subject.last_name = nil
+    expect(subject).to_not be_valid
   end
 
-  describe "get edit_customer_path" do
-    it "renders the :edit template" do
-      customer = FactoryBot.create(:customer)
-      get edit_customer_path(id: customer.id)
-      expect(response).to render_template(:edit)
-    end
+  it "is not valid without a phone number" do
+    subject.phone = nil
+    expect(subject).to_not be_valid
   end
 
-  describe "post customers_path with valid data" do
-    it "saves a new entry and redirects to the show path for the entry" do
-      customer_attributes = FactoryBot.attributes_for(:customer)
-      expect { post customers_path, params: { customer: customer_attributes } }.to change(Customer, :count)
-      expect(response).to redirect_to customer_path(id: Customer.last.id)
-    end
+  it "is not valid without an email" do
+    subject.email = nil
+    expect(subject).to_not be_valid
   end
 
-  describe "post customers_path with invalid data" do
-    it "does not save a new entry or redirect" do
-      customer_attributes = FactoryBot.attributes_for(:customer)
-      customer_attributes.delete(:first_name)
-      expect { post customers_path, params: { customer: customer_attributes } }.to_not change(Customer, :count)
-      expect(response).to render_template(:new)
-    end
+  it "is not valid if the phone number is not 10 chars" do
+    subject.phone = "12345"
+    expect(subject).to_not be_valid
   end
 
-  describe "put customer_path with valid data" do
-    it "updates an entry and redirects to the show path for the customer" do
-      customer = FactoryBot.create(:customer)
-      new_attributes = FactoryBot.attributes_for(:customer, first_name: "Updated Name")
-      put customer_path(id: customer.id), params: { customer: new_attributes }
-      expect(response).to redirect_to customer_path(id: customer.id)
-      customer.reload
-      expect(customer.first_name).to eq("Updated Name")
-    end
+  it "is not valid if the phone number is not all digits" do
+    subject.phone = "888999ABC"
+    expect(subject).to_not be_valid
   end
 
-  describe "put customer_path with invalid data" do
-    it "does not update the customer record or redirect" do
-      customer = FactoryBot.create(:customer)
-      invalid_attributes = FactoryBot.attributes_for(:customer, first_name: nil)
-      put customer_path(id: customer.id), params: { customer: invalid_attributes }
-      expect(response).to render_template(:edit)
-      customer.reload
-      expect(customer.first_name).not_to be_nil
-    end
+  it "is not valid if the email address doesn't have a @" do
+    subject.email = "jsmithsample.com"
+    expect(subject).to_not be_valid
   end
 
-  describe "delete a customer record" do
-    it "deletes a customer record" do
-      customer = FactoryBot.create(:customer)
-      expect { delete customer_path(id: customer.id) }.to change(Customer, :count).by(-1)
-      expect(response).to redirect_to customers_path
-    end
+  it "returns the correct full_name" do
+    expect(subject.full_name).to eq("Jack Smith")
   end
 end
